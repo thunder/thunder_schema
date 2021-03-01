92086637d5268c2cb6035db32d9e239cad38727d
Daniel Bosen <daniel.bosen@burda.com>
2021-03-01 09:53:13 +0100

    Rename back to media

--- src/Plugin/GraphQL/SchemaExtension/ThunderMediaSchemaExtension.php
+++ src/Plugin/GraphQL/SchemaExtension/ThunderMediaSchemaExtension.php
@@ -37,22 +37,22 @@ class ThunderMediaSchemaExtension extends ThunderSchemaExtensionPluginBase {
   protected function resolveFields() {
 
     // Image
-    $this->resolveBaseFields('MediaImage');
-    $this->registry->addFieldResolver('MediaImage', 'copyright',
+    $this->resolveBaseFields('Image');
+    $this->registry->addFieldResolver('Image', 'copyright',
       $this->builder->produce('property_path')
         ->map('type', $this->builder->fromValue('entity:media'))
         ->map('value', $this->builder->fromParent())
         ->map('path', $this->builder->fromValue('field_copyright.value'))
     );
 
-    $this->registry->addFieldResolver('MediaImage', 'description',
+    $this->registry->addFieldResolver('Image', 'description',
       $this->builder->produce('property_path')
         ->map('type', $this->builder->fromValue('entity:media'))
         ->map('value', $this->builder->fromParent())
         ->map('path', $this->builder->fromValue('field_description.processed'))
     );
 
-    $this->registry->addFieldResolver('MediaImage', 'src',
+    $this->registry->addFieldResolver('Image', 'src',
       $this->builder->compose(
         $this->builder->produce('property_path')
           ->map('type', $this->builder->fromValue('entity:media'))
@@ -63,35 +63,35 @@ class ThunderMediaSchemaExtension extends ThunderSchemaExtensionPluginBase {
       )
     );
 
-    $this->registry->addFieldResolver('MediaImage', 'width',
+    $this->registry->addFieldResolver('Image', 'width',
       $this->builder->produce('property_path')
         ->map('type', $this->builder->fromValue('entity:media'))
         ->map('value', $this->builder->fromParent())
         ->map('path', $this->builder->fromValue('field_image.width'))
     );
 
-    $this->registry->addFieldResolver('MediaImage', 'height',
+    $this->registry->addFieldResolver('Image', 'height',
       $this->builder->produce('property_path')
         ->map('type', $this->builder->fromValue('entity:media'))
         ->map('value', $this->builder->fromParent())
         ->map('path', $this->builder->fromValue('field_image.height'))
     );
 
-    $this->registry->addFieldResolver('MediaImage', 'title',
+    $this->registry->addFieldResolver('Image', 'title',
       $this->builder->produce('property_path')
         ->map('type', $this->builder->fromValue('entity:media'))
         ->map('value', $this->builder->fromParent())
         ->map('path', $this->builder->fromValue('field_image.title'))
     );
 
-    $this->registry->addFieldResolver('MediaImage', 'alt',
+    $this->registry->addFieldResolver('Image', 'alt',
       $this->builder->produce('property_path')
         ->map('type', $this->builder->fromValue('entity:media'))
         ->map('value', $this->builder->fromParent())
         ->map('path', $this->builder->fromValue('field_image.alt'))
     );
 
-    $this->registry->addFieldResolver('MediaImage', 'tags',
+    $this->registry->addFieldResolver('Image', 'tags',
       $this->builder->produce('entity_reference')
         ->map('entity', $this->builder->fromParent())
         ->map('field', $this->builder->fromValue('field_tags'))
