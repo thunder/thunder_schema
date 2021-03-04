14087171f3d63bd7fcf1ee789e21a6ca368354c9
Daniel Bosen <daniel.bosen@burda.com>
2021-03-04 17:29:32 +0100

    add minimal tests to test testing

---
+++ tests/src/Functional/ThunderGqlsTestBase.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\Tests\thunder_gqls\Functional;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * The base class for all functional Thunder GraphQl schema tests.
+ */
+abstract class ThunderGqlsTestBase extends BrowserTestBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $profile = 'thunder';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'thunder_gqls',
+  ];
+
+  /**
+   * User with graphql request privileges.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $graphqlUser;
+
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->graphqlUser = $this->drupalCreateUser([
+      'execute thunder_graphql arbitrary graphql requests',
+      'access content',
+      'access user profiles',
+      'view media',
+      'view published terms in channel',
+      'view published terms in tags'
+    ]);
+
+  }
+
+  /**
+   * Login with defined role assigned to user.
+   *
+   * @param string $role
+   *   Role name that will be assigned to user.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function logWithRole(string $role) {
+    $user = $this->drupalCreateUser();
+        $user->addRole($role);
+    $user->save();
+    $this->drupalLogin($user);
+  }
+}
