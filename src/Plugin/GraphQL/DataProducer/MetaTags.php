add24a4068888c48cb5ef1d1f46d4ae950aad5cb
Daniel Bosen <daniel.bosen@burda.com>
2021-03-25 15:15:38 +0100

    remove dependency on metatag patch

--- src/Plugin/GraphQL/DataProducer/MetaTags.php
+++ src/Plugin/GraphQL/DataProducer/MetaTags.php
@@ -3,7 +3,10 @@
 namespace Drupal\thunder_gqls\Plugin\GraphQL\DataProducer;
 
 use Drupal\Component\Serialization\Json;
+use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Cache\RefinableCacheableDependencyInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Render\BubbleableMetadata;
 use Drupal\Core\Render\RenderContext;
@@ -11,6 +14,7 @@ use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\TypedData\TypedDataInterface;
 use Drupal\Core\TypedData\TypedDataTrait;
 use Drupal\graphql\Plugin\GraphQL\DataProducer\DataProducerPluginBase;
+use Drupal\metatag\MetatagManagerInterface;
 use Drupal\typed_data\DataFetcherTrait;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -47,6 +51,20 @@ class MetaTags extends DataProducerPluginBase implements ContainerFactoryPluginI
   protected $renderer;
 
   /**
+   * The metatag manager service.
+   *
+   * @var \Drupal\metatag\MetatagManagerInterface
+   */
+  protected $metatagManager;
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
    * {@inheritdoc}
    *
    * @codeCoverageIgnore
@@ -56,7 +74,9 @@ class MetaTags extends DataProducerPluginBase implements ContainerFactoryPluginI
       $configuration,
       $pluginId,
       $pluginDefinition,
-      $container->get('renderer')
+      $container->get('renderer'),
+      $container->get('metatag.manager'),
+      $container->get('module_handler')
     );
   }
 
@@ -71,21 +91,30 @@ class MetaTags extends DataProducerPluginBase implements ContainerFactoryPluginI
    *   The plugin definition.
    * @param \Drupal\Core\Render\RendererInterface $renderer
    *   The renderer service.
+   * @param \Drupal\metatag\MetatagManagerInterface $metatagManager
+   *   The metatag manager service.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
+   *   The module handler.
    */
   public function __construct(
     array $configuration,
     string $pluginId,
     $pluginDefinition,
-    RendererInterface $renderer
+    RendererInterface $renderer,
+    MetatagManagerInterface $metatagManager,
+    ModuleHandlerInterface $moduleHandler
+
   ) {
     parent::__construct($configuration, $pluginId, $pluginDefinition);
     $this->renderer = $renderer;
+    $this->metatagManager = $metatagManager;
+    $this->moduleHandler = $moduleHandler;
   }
 
   /**
    * Resolve the metadata.
    *
-   * @param mixed $metatags
+   * @param mixed $value
    *   The root value.
    * @param string|null $type
    *   The root type.
@@ -95,33 +124,43 @@ class MetaTags extends DataProducerPluginBase implements ContainerFactoryPluginI
    * @return mixed
    *   Normalized metatags.
    */
-  public function resolve($metatags, ?string $type, RefinableCacheableDependencyInterface $metadata) {
-    if (!($metatags instanceof TypedDataInterface) && !empty($type)) {
-      $manager = $this->getTypedDataManager();
-      $definition = $manager->createDataDefinition($type);
-      $metatags = $manager->create($definition, $metatags);
-    }
+  public function resolve($value, ?string $type, RefinableCacheableDependencyInterface $metadata) {
+    if ($value instanceof ContentEntityInterface) {
+      $context = new RenderContext();
+      $result = $this->renderer->executeInRenderContext($context, function () use ($value) {
+        $tags = $this->metatagManager->tagsFromEntityWithDefaults($value);
 
-    if (!($metatags instanceof TypedDataInterface)) {
-      throw new \BadMethodCallException('Could not derive typed data type.');
-    }
+        // Trigger hook_metatags_attachments_alter().
+        // Allow modules to rendered metatags prior to attaching.
+        $this->moduleHandler->alter('metatags_attachments', $tags);
 
-    $bubbleable = new BubbleableMetadata();
-    $fetcher = $this->getDataFetcher();
+        // Filter non schema metatags, because schema metatags are processed in
+        // EntitySchemaMetatags class.
+        $elements = $this->metatagManager->generateRawElements($tags, $value);
+        $elements = array_filter(
+          $elements,
+          function ($metatag_object) {
+            return !NestedArray::getValue(
+              $metatag_object,
+              [
+                '#attributes',
+                'schema_metatag',
+              ]
+            );
+          }
+        );
 
-    $context = new RenderContext();
-    $path = 'metatag';
+        $data = [];
+        foreach ($elements as $element) {
+          $data[] = ['tag' => $element['#tag'], 'attributes' => Json::encode($element['#attributes'])];
+        }
 
-    $result = $this->renderer->executeInRenderContext($context, function () use ($fetcher, $metatags, $path, $bubbleable) {
-      $metatags = $fetcher->fetchDataByPropertyPath($metatags, $path, $bubbleable)->getValue();
-      foreach ($metatags as $key => $metatag) {
-        $metatags[$key]['attributes'] = Json::encode($metatag['attributes']);
+        return $data;
+      });
+
+      if (!$context->isEmpty()) {
+        $metadata->addCacheableDependency($context->pop());
       }
-      return $metatags;
-    });
-
-    if (!$context->isEmpty()) {
-      $metadata->addCacheableDependency($context->pop());
     }
 
     return $result ?? NULL;
